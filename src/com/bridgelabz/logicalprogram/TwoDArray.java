11493b551a47961a1e47f3eb11fbab2a20c24ac4
HemnathG <hemnathdavid@gmail.com>
2022-03-13 19:50:27 +0530

    Adding files

---
+++ src/com/bridgelabz/logicalprogram/TwoDArray.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.logicalprogram;
+
+import java.util.Scanner;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+
+        int[][] arr = new int[3][3];
+        Scanner sc = new Scanner(System.in);
+        for (int m = 0; m < 3; m++) {
+            for (int n = 0; n < 3; n++) {
+                System.out.print("Enter Values for array : ");
+
+                arr[m][n] = sc.nextInt();
+                System.out.println();
+            }
+        }
+
+        for (int m = 0; m < 3; m++) {
+            System.out.println();
+            for (int n = 0; n < 3; n++) {
+                System.out.print(arr[m][n] + " ");
+            }
+        }
+    }
+}
